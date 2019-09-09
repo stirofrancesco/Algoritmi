2aff51bc2d2e3626c764e1894d1e1863261e2df9
stirofrancesco <40853631+stirofrancesco@users.noreply.github.com>
2019-09-09 12:58:57 +0200

    Problema Del Resto
    Numero minimo di monete necessario a restituire un resto esatto pari a R utilizzando la programmazione dinamica

---
+++ resto/resto.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int resto(int* tagli, int n, int r){
+
+  int* table = new int[r+1];
+  table[0] = 0;
+  for (int i=1;i<=r;i++)
+    table[i] = 10000;
+
+  for (int i=1;i<=r;i++)
+    for(int j=0;j<n;j++){
+      if(i-tagli[j]>=0)
+        table[i] = min(table[i],1+table[i-tagli[j]]);
+    }
+  return table[r];
+}
+
+
+int main(){
+  ifstream input;
+  ofstream output;
+  input.open("input.txt");
+  output.open("output.txt");
+
+  int r,n;
+
+  for (int i=0;i<100;i++){
+    input >> r >> n;
+    int* array = new int[n];
+    for(int j=0;j<n;j++)
+      input >> array[j];
+
+    output << resto(array,n,r) << endl;
+  }
+}
