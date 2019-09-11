a502c617b5fac18ae50def35e2db75c4d6a11342
stirofrancesco <40853631+stirofrancesco@users.noreply.github.com>
2019-09-11 08:51:31 +0200

    Cammini parziali Bellman-Ford

---
+++ bellman-ford-p/bellmanfordp.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <fstream>
+#include <limits.h>
+
+using namespace std;
+
+template <class H> class Graph{
+  private:
+    H** K;
+    int** adj;
+    int n,len;
+
+    int findIndex(H x){
+      for (int i=0;i<len;i++)
+        if(K[i] && *K[i]==x) return i;
+      return -1;
+    }
+
+  public:
+    Graph(int dim){
+      n = dim;
+      len = 0;
+      K = new H*[n];
+      for(int i=0;i<n;i++)
+        K[i] = NULL;
+
+      adj = new int*[n];
+      for (int i=0;i<n;i++){
+        adj[i] = new int[n];
+        for(int j=0;j<n;j++)
+          adj[i][j] = INT_MIN;
+      }
+    }
+
+    Graph<H>* addNode(H x){
+      if(len==n) return this;
+      if(findIndex(x)>=0) return this;
+      K[len++] = new H(x);
+      return this;
+    }
+
+    Graph<H>* addEdge(H x, H y, int weight){
+      int i = findIndex(x);
+      int j = findIndex(y);
+      if(i<0 || j<0) return this;
+      if(adj[i][j] == INT_MIN)
+        adj[i][j] = weight;
+      return this;
+    }
+
+    void BellmanFord(H sorgente, H destinazione, int c, ofstream& out){
+      int* dist = new int[len];
+      for(int i=0;i<len;i++)
+        dist[i] = INT_MAX;
+      dist[findIndex(sorgente)] = 0;
+
+      for(int k=0;k<c;k++)
+        for(int i=0;i<len;i++)
+          for(int j=0;j<len;j++)
+            if(adj[i][j]!=INT_MIN){
+              int weight = adj[i][j];
+              if(dist[i]!=INT_MAX && dist[i]+weight<dist[j])
+                dist[j] = dist[i]+weight;
+            }
+
+
+    if(dist[findIndex(destinazione)]==INT_MAX) out << "inf." << endl;
+    else out << dist[findIndex(destinazione)] << endl;
+  }
+
+};
+
+int main(){
+  ifstream input;
+  ofstream output;
+  input.open("input.txt");
+  output.open("output.txt");
+
+  int n,m,c;
+
+  for (int i=0;i<100;i++){
+    input >> n >> m >> c;
+    Graph<int>* g = new Graph<int>(n);
+    for (int j=0;j<n;j++)
+      g->addNode(j);
+
+    for(int j=0;j<m;j++){
+      char tmp; input >> tmp;
+      int f; input >> f;
+      int s; input >> s;
+      int p; input >> p;
+      input >> tmp;
+      g->addEdge(f,s,p);
+    }
+    int sorgente; input >> sorgente;
+    int destinazione; input >> destinazione;
+    g->BellmanFord(sorgente,destinazione,c,output);
+ }
+}
